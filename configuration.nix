0317e846f2c24225d67100a798bcbba62fbbb163
Tyler Alterio <tyalt1@gmail.com>
2024-03-25 21:57:30 -0400

    add blank readme, seperate virtual box and main config

--- configuration.nix
+++ configuration.nix
@@ -2,47 +2,24 @@
 
 {
   imports = [
-    <nixpkgs/nixos/modules/installer/virtualbox-demo.nix>
   ];
 
   # Let demo build as a trusted user.
-# nix.settings.trusted-users = [ "demo" ];
+  # nix.settings.trusted-users = [ "demo" ];
 
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
 
-
-# Mount a VirtualBox shared folder.
-# This is configurable in the VirtualBox menu at
-# Machine / Settings / Shared Folders.
-# fileSystems."/mnt" = {
-#   fsType = "vboxsf";
-#   device = "nameofdevicetomount";
-#   options = [ "rw" ];
-# };
-
-# By default, the NixOS VirtualBox demo image includes SDDM and Plasma.
-# If you prefer another desktop manager or display manager, you may want
-# to disable the default.
-# services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
-# services.xserver.displayManager.sddm.enable = lib.mkForce false;
-
-# Enable GDM/GNOME by uncommenting above two lines and two lines below.
-# services.xserver.displayManager.gdm.enable = true;
-# services.xserver.desktopManager.gnome.enable = true;
-
-# Set your time zone.
+  # Set your time zone.
   time.timeZone = "America/New_York";
 
   nixpkgs.config.allowUnfree = true;
 
-# List packages installed in system profile. To search, run:
-# \$ nix search wget
   environment.systemPackages = with pkgs; [
     # nix packages
-    nixpkgs-fmt    
+    nixpkgs-fmt
 
     # cli utils
     wget
@@ -60,15 +37,5 @@
     obs-studio
     spotify
   ];
-
-#  home.file.".tool-versions" = {
-#    executable = false;
-#    text = ''
-#    python 3.12.2
-#    '';
-#  };
-
-# Enable the OpenSSH daemon.
-# services.openssh.enable = true;
 
 }
---
+++ hosts/virtualbox.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    <nixpkgs/nixos/modules/installer/virtualbox-demo.nix>
+  ];
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # Let demo build as a trusted user.
+  # nix.settings.trusted-users = [ "demo" ];
+
+  time.timeZone = "America/New_York";
+
+  environment.systemPackages = with pkgs; [
+    # cli utils
+    wget
+    vim
+    jq
+    git
+
+    # dev tools
+    asdf-vm # version manager
+    vscode
+  ];
+
+  # Mount a VirtualBox shared folder.
+  # This is configurable in the VirtualBox menu at
+  # Machine / Settings / Shared Folders.
+  # fileSystems."/mnt" = {
+  #   fsType = "vboxsf";
+  #   device = "nameofdevicetomount";
+  #   options = [ "rw" ];
+  # };
+
+  # By default, the NixOS VirtualBox demo image includes SDDM and Plasma.
+  # If you prefer another desktop manager or display manager, you may want
+  # to disable the default.
+  # services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
+  # services.xserver.displayManager.sddm.enable = lib.mkForce false;
+
+  # Enable GDM/GNOME by uncommenting above two lines and two lines below.
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+}
